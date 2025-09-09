1d1cca1655708fb60cfa2a66665dc258b9438331
matanya alkobi <matanor123456@gmail.com>
2025-09-09 15:26:16 +0300

    Implementing functionality of fetching a post by clicking on it - Retrieving the id using useParams - Adding a redirect to clicking on the post - Creating a page that displays the post - Adding a PostPage function that displays the requested post to the client

--- src/pages/AllPostsPage.tsx
+++ src/pages/AllPostsPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getPostHandler } from "../controller/post.controller";
 import PostCreator from "../components/PostCreator";
 import { type Post } from "../interfaces/Post";
-import "../styles/postPage.css"
+import "../styles/allPostsPage.css"
 
 // Calls the function getPostHandler that fetches the posts,
 // and creates posts in a loop by calling the function PostCreator
@@ -10,9 +10,8 @@ export default  function PostsPage(){
       const [posts, setPosts] = useState<Post[]>([]);
       useEffect(() => {
         const fetchPosts = async () => {
-          const data = await getPostHandler();
-          setPosts(data);
-          console.log(data);
+          const AllPosts = await getPostHandler();
+          setPosts(AllPosts);
         };
         fetchPosts();
       }, []);
