98e7c41d9e1c0f7a54e0a19a602e3824e73d74e5
matanya alkobi <matanor123456@gmail.com>
2025-09-09 12:33:24 +0300

    Designing the search page for a single post

--- src/components/application-layout/appLayout.tsx
+++ src/components/application-layout/appLayout.tsx
@@ -11,12 +11,19 @@ export default function Layout() {
           <GetImage imgSrc="/images/app-logo.jpeg" alt="app-logo-image" />
         </div>
       </header>
-      <div className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="postSearch">post search</Link>
-      </div>
-      <div>
-        <Outlet />
+
+      <div className="page-structure">
+        <div className="navbar">
+          <Link className="navbar-link" to="/">
+            Home
+          </Link>
+          <Link className="navbar-link" to="postSearch">
+            post search
+          </Link>
+        </div>
+        <div className="outlet">
+          <Outlet />
+        </div>
       </div>
     </>
   );
