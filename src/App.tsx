24d07bcf9e871e41e99a22f5767083735306f3d9
matanya alkobi <matanor123456@gmail.com>
2025-09-10 17:44:42 +0300

    End of login task - When client tries to log in - Sends fetch to server - Adds cookie to all fetches

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import AllpostsPage from "./pages/AllPostsPage.tsx";
 import GetPostByID from "./pages/PostSearchPage.tsx";
 import PostPage from "./pages/postPage.tsx";
 import CreateAPostPage from "./pages/CreateAPostPage.tsx";
+import LoginAndRegistration from "./pages/LoginAndRegistration.tsx";
 
 function App() {
   return (
     <Routes>
+      <Route  path="/" element={<LoginAndRegistration />}/>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<AllpostsPage />} />
+        <Route path="/home" element={<AllpostsPage />} />
         <Route path="/postsearch" element={<GetPostByID />} />
         <Route path="/posts/:id" element={<PostPage />} />
         <Route path="/createPost" element={<CreateAPostPage />} />
